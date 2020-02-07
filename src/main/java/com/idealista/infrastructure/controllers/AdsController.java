d20e6f854184e50470e7f9479bbe5f582ce62a33
Jonathan <j.navajo@alumnos.upm.es>
2020-02-07 10:26:49 +0100

    feat: added requestMappings in controller

--- src/main/java/com/idealista/infrastructure/controllers/AdsController.java
+++ src/main/java/com/idealista/infrastructure/controllers/AdsController.java
@@ -2,7 +2,12 @@ package com.idealista.infrastructure.controllers;
 
 import com.idealista.application.entity.PublicAd;
 import com.idealista.application.entity.QualityAd;
+import com.idealista.application.entity.ScoreAd;
+import com.idealista.application.service.IAdsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,21 +15,24 @@ import java.util.List;
 @RestController
 public class AdsController {
 
-    //TODO añade url del endpoint
+    @Autowired
+    private IAdsService adsService;
+
+    @GetMapping("/qualityAds")
     public ResponseEntity<List<QualityAd>> qualityListing() {
         //TODO rellena el cuerpo del método
-        return ResponseEntity.notFound().build();
+        return new ResponseEntity<List<QualityAd>>(adsService.findQualityAds(), HttpStatus.OK);
     }
 
-    //TODO añade url del endpoint
+    @GetMapping("/publicAds")
     public ResponseEntity<List<PublicAd>> publicListing() {
         //TODO rellena el cuerpo del método
-        return ResponseEntity.notFound().build();
+        return new ResponseEntity<List<PublicAd>>(adsService.findPublicAds(), HttpStatus.OK);
     }
 
-    //TODO añade url del endpoint
-    public ResponseEntity<Void> calculateScore() {
+    @GetMapping("/scoreAds")
+    public ResponseEntity<List<ScoreAd>> calculateScore() {
         //TODO rellena el cuerpo del método
-        return ResponseEntity.notFound().build();
+        return new ResponseEntity<List<ScoreAd>>(adsService.calculateScore(), HttpStatus.OK);
     }
 }

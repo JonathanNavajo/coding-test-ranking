4b65519b8678bbcb1fb7ee6d3c674b47a86178d6
Jonathan <j.navajo@alumnos.upm.es>
2020-02-08 09:58:10 +0100

    feat: added method for irrelevant ads in service

--- src/main/java/com/idealista/application/service/AdsServiceImpl.java
+++ src/main/java/com/idealista/application/service/AdsServiceImpl.java
@@ -1,13 +1,16 @@
 package com.idealista.application.service;
 
+import com.idealista.application.entity.AdVO;
 import com.idealista.application.entity.PublicAd;
 import com.idealista.application.entity.QualityAd;
 import com.idealista.application.entity.ScoreAd;
-import com.idealista.application.mapper.PublicAdsMapper;
+import com.idealista.application.mapper.AdsMapper;
 import com.idealista.infrastructure.persistence.InMemoryPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,21 +20,32 @@ public class AdsServiceImpl implements IAdsService {
     @Autowired
     private InMemoryPersistence inMemoryPersistence;
 
-    private PublicAdsMapper publicAdsMapper;
-
     @Override
     public List<PublicAd> findPublicAds() {
-        return inMemoryPersistence.getAds().stream().filter(item -> item.getScore() != null && item.getScore() > 40)
-                .map(item -> publicAdsMapper.mapAdVOToPublicAd(item, inMemoryPersistence.getPictures())).collect(Collectors.toList());
+
+        // Sort AdsList based on the score
+        Collections.sort(inMemoryPersistence.getAds(), (o1, o2) -> new Integer(o2.getScore()).compareTo(new Integer(o1.getScore())));
+
+        return inMemoryPersistence.getAds()
+                .stream()
+                .filter(item -> item.getScore() != null && item.getScore() > 40)
+                .map(item -> AdsMapper.mapAdVOToPublicAd(item, inMemoryPersistence.getPictures()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<QualityAd> findQualityAds() {
-        return null;
+
+        return inMemoryPersistence.getAds()
+                .stream()
+                .filter(item -> item.getScore() != null && item.getScore() < 40)
+                .map(item -> AdsMapper.mapAdVOToQualityAd(item, inMemoryPersistence.getPictures()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<ScoreAd> calculateScore() {
         return null;
     }
+
 }

684934320b31369f4fec872e99f55872fd126395
Jonathan <j.navajo@alumnos.upm.es>
2020-02-08 09:57:41 +0100

    feat: added mapper for adVo to QualityAd

--- src/main/java/com/idealista/application/mapper/AdsMapper.java
+++ src/main/java/com/idealista/application/mapper/AdsMapper.java
@@ -3,18 +3,24 @@ package com.idealista.application.mapper;
 import com.idealista.application.entity.AdVO;
 import com.idealista.application.entity.PictureVO;
 import com.idealista.application.entity.PublicAd;
+import com.idealista.application.entity.QualityAd;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class PublicAdsMapper {
+public class AdsMapper {
 
-    public PublicAd mapAdVOToPublicAd(AdVO adVO, List<PictureVO> pictureVOS) {
+    public static PublicAd mapAdVOToPublicAd(AdVO adVO, List<PictureVO> pictureVOS) {
         return new PublicAd(adVO.getId(), adVO.getTypology(), adVO.getDescription(), mapPicturesIntegerToString(adVO.getPictures(), pictureVOS)
                 , adVO.getHouseSize(), adVO.getGardenSize());
     }
 
-    private List<String> mapPicturesIntegerToString(List<Integer> pictures, List<PictureVO> pictureVOS) {
+    public static QualityAd mapAdVOToQualityAd(AdVO adVO, List<PictureVO> pictureVOS) {
+        return new QualityAd(adVO.getId(), adVO.getTypology(), adVO.getDescription(), mapPicturesIntegerToString(adVO.getPictures(), pictureVOS)
+                , adVO.getHouseSize(), adVO.getGardenSize(), adVO.getScore(), adVO.getIrrelevantSince());
+    }
+
+    private static List<String> mapPicturesIntegerToString(List<Integer> pictures, List<PictureVO> pictureVOS) {
         return pictures.stream().map(item -> {
             return pictureVOS.stream().filter(pictureVO -> pictureVO.getId().equals(item)).map(PictureVO::getUrl).findFirst().orElse(null);
         }).collect(Collectors.toList());

73c13ba5e80e58781e656f5f99ea7d984b57bdb2
Jonathan <jonatan.navajo@soprasteria.com>
2020-02-10 11:21:43 +0100

    fix: change return in controllers

--- src/main/java/com/idealista/infrastructure/controllers/AdsController.java
+++ src/main/java/com/idealista/infrastructure/controllers/AdsController.java
@@ -1,8 +1,8 @@
 package com.idealista.infrastructure.controllers;
 
+import com.idealista.application.entity.AdVO;
 import com.idealista.application.entity.PublicAd;
 import com.idealista.application.entity.QualityAd;
-import com.idealista.application.entity.ScoreAd;
 import com.idealista.application.service.IAdsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,18 +21,18 @@ public class AdsController {
     @GetMapping("/qualityAds")
     public ResponseEntity<List<QualityAd>> qualityListing() {
         //TODO rellena el cuerpo del método
-        return new ResponseEntity<List<QualityAd>>(adsService.findQualityAds(), HttpStatus.OK);
+        return new ResponseEntity<>(adsService.findQualityAds(), HttpStatus.OK);
     }
 
     @GetMapping("/publicAds")
     public ResponseEntity<List<PublicAd>> publicListing() {
         //TODO rellena el cuerpo del método
-        return new ResponseEntity<List<PublicAd>>(adsService.findPublicAds(), HttpStatus.OK);
+        return new ResponseEntity<>(adsService.findPublicAds(), HttpStatus.OK);
     }
 
     @GetMapping("/scoreAds")
-    public ResponseEntity<List<ScoreAd>> calculateScore() {
+    public ResponseEntity<List<AdVO>> calculateScore() {
         //TODO rellena el cuerpo del método
-        return new ResponseEntity<List<ScoreAd>>(adsService.calculateScore(), HttpStatus.OK);
+        return new ResponseEntity<>(adsService.calculateScore(), HttpStatus.OK);
     }
 }
